41deb75bd6aed548766dcd5189d97632fcf66f5e
恨别鸟惊心_ <3346926171@qq.com>
2023-08-08 22:33:25 +0800

    初次提交

--- README.md
+++ README.md
@@ -1,52 +1,17 @@
-# LiteLoaderQQNT-Plugin-Template
+# LiteLoaderQQNT-Message-Show
 
-LiteLoaderQQNT的插件模板，包含插件创建方法，编写建议，还有技巧  
-LiteLoaderQQNT本体：[LiteLoaderQQNT](https://github.com/mo-jinran/LiteLoaderQQNT)
+[LiteLoaderQQNT](https://github.com/mo-jinran/LiteLoaderQQNT) 插件
 
-Telegram闲聊群：https://t.me/LiteLoaderQQNT
+仓库创建自 [LiteLoaderQQNT-Plugin-Template](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template)
 
-
-# 关于插件从 V2 更新至 V3 说明 
-
-各位插件开发者，按照这条提示来更新插件    
-建议参考其他已上架插件市场的插件
-
-以下是插件需要的改动  
-- 更改manifest.json文件，manifest_version改为3
-- 彻底移除了旧版betterQQNT对象，请使用LiteLoader对象
-- 主进程脚本onLoad函数不再提供LiteLoader对象，你可以在主进程环境任何地方使用LiteLoader对象
-- 因为Windows QQNT渲染进程无法使用file://协议请求文件，请替换为llqqnt://local-file/
-
-对于上架插件市场的插件，建议开单独分支  
-方便插件市场获取新与旧的插件仓库并更新插件  
-（这些不是必须的，如果你不打算保留旧版，可单分支冲过去（  
-可继续使用Repo方式 ，或使用Release来上传插件  
-Release附加的插件文件必须是.zip格式  
-新版（v3）需压缩插件目录内部必要的文件  
-然后在插件仓库提交issue或pr在v3分支（pr的请写在最开始）
+## 功能
+- 消息连续显示
+    - 来自同一用户相同时段内的消息，将不会重复显示用户头像及昵称，消息气泡将会连续显示
+    - 功能实现函数来自[MUKAPP/LiteLoaderQQNT-MSpring-Theme](https://github.com/MUKAPP/LiteLoaderQQNT-MSpring-Theme)
 
 
-# 插件编写指南文档
+## 开源协议
+[MIT License](./LICENSE)  
+Copyright (c) 2023 恨别鸟惊心_
 
-文档目前还不算完善，但能看，也欢迎各位来完善/修复错误
-
-- [了解数据目录结构](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/1.了解数据目录结构)
-    - [LiteLoader的数据目录](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/1.了解数据目录结构#liteloader的数据目录)
-        - [plugins](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/1.了解数据目录结构#plugins)
-        - [plugins_data](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/1.了解数据目录结构#plugins_data)
-        - [plugins_cache](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/1.了解数据目录结构#plugins_cache)
-        - [config.json](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/1.了解数据目录结构#config.json)
-- [了解插件目录结构](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/1.了解插件目录结构)
-    - [插件的目录](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/2.了解插件目录结构#插件的目录)
-        - [manifest.json](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/2.了解插件目录结构#manifest.json)
-        - [README.md](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/2.了解插件目录结构#README.md)
-        - [LICENSE](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/2.了解插件目录结构#LICENSE)
-        - [main.js](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/2.了解插件目录结构#main.js)
-        - [preload.js](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/2.了解插件目录结构#preload.js)
-        - [renderer.js](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/2.了解插件目录结构#renderer.js)
-- [插件基本代码结构](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/3.插件基本代码结构)
-    - [插件的结构](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/3.插件基本代码结构#插件的结构)
-        - [Manifest](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/manifest.json)
-        - [主进程](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/main.js)
-        - [预加载](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/preload.js)
-        - [渲染进程](https://github.com/mo-jinran/LiteLoaderQQNT-Plugin-Template/wiki/renderer.js)
+本插件仅供个人学习使用。请遵纪守法，后果自负
--- src/renderer.js
+++ src/renderer.js
@@ -1,9 +1,111 @@
 // 运行在 Electron 渲染进程 下的页面脚本
 
+// 同一个人消息连续显示
+// 函数复制自 MUKAPP/LiteLoaderQQNT-MSpring-Theme
+function concatBubble() {
+    const msgList = document.querySelector('#ml-root .ml-list');
+
+    if (msgList) {
+        function compareTwoMsg(lower, upper) {
+            return new Promise((resolve, reject) => {
+                try {
+                    // 检查lower是否包含timeStamp, gray-message
+                    if (lower.querySelector(".gray-tip-message,.message__timestamp")) {
+                        resolve();
+                        return;
+                    }
+                    // 检查upper和lower是否包含撤回, 检测message-container
+                    if (!lower.querySelector(".message-container") || !upper.querySelector(".message-container")) {
+                        resolve();
+                        return;
+                    }
+                    const avatarLower = lower.querySelector("span.avatar-span");
+                    const avatarUpper = upper.querySelector("span.avatar-span");
+                    // const usernameNodeLower = lower.querySelector("span.avatar-span");
+                    const usernameNodeLower = lower.querySelector("div.user-name");
+                    const usernameLower = avatarLower.getAttribute("aria-label");
+                    const usernameUpper = avatarUpper.getAttribute("aria-label");
+                    const containerLower = lower.querySelector("div.msg-content-container")
+                    if (usernameLower === usernameUpper) {
+                        const bubbleLower = lower.querySelector("div.msg-content-container");
+                        // 删除upper message的paddingBottom
+                        upper.style.paddingBottom = "0";
+                        // 删除upper message-container的paddingBottom
+                        upper.querySelector("div.message-container").style.paddingBottom = "0";
+                        // upper message-container的paddingTop为4px
+                        upper.querySelector("div.message-container").style.paddingTop = "4px";
+                        // lower message-container的paddingTop为4px
+                        lower.querySelector("div.message-container").style.paddingTop = "4px";
+                        // lower头像调透明
+                        avatarLower.style.opacity = "0";
+                        // lower的username 不显示
+                        if (usernameNodeLower && usernameNodeLower.style) {
+                            usernameNodeLower.style.marginBottom = "0";
+                            usernameNodeLower.style.display = "none";
+                        }
+
+                    }
+                    resolve();
+                } catch (error) {
+                    log("compareMessage Error", error)
+                    // log("lower", lower.innerHTML)
+                    // log("upper", upper.innerHTML)
+                    // 不reject, 避免影响其他任务
+                    resolve();
+                }
+            });
+        }
+
+        let lastMessageNodeList = Array.from(msgList.querySelectorAll("div.message"));
+
+        const observer = new MutationObserver(async function () {
+            // 比对两轮的msgList
+            let currMessageNodeList = Array.from(msgList.querySelectorAll("div.message"));
+            let lastMessageNodeSet = new Set(lastMessageNodeList);
+
+            let tasks = [];
+            for (let i = 0; i < currMessageNodeList.length - 1; i++) {
+                let currMsg = currMessageNodeList[i];
+                if (!lastMessageNodeSet.has(currMsg)) {
+                    tasks.push(compareTwoMsg(currMessageNodeList[i], currMessageNodeList[i + 1]));
+                }
+            }
+            // 提速
+            Promise.all(tasks).then(() => {
+                // log("Promise all complete")
+            }).catch(() => {
+                log("Promise not complete all")
+            });
+
+            lastMessageNodeList = currMessageNodeList;
+        });
+        const config = { childList: true };
+        observer.observe(msgList, config);
+    }
+}
+
+function observeElement(selector, callback, callbackEnable = true, interval = 100, timeout = 5000) {
+    let elapsedTime = 0;
+    const timer = setInterval(function () {
+        const element = document.querySelector(selector);
+        if (element) {
+            if (callbackEnable) {
+                callback();
+            }
+            clearInterval(timer);
+        }
+
+        elapsedTime += interval;
+        if (elapsedTime >= timeout) {
+            clearInterval(timer);
+            log('超时', selector, "未出现");
+        }
+    }, interval);
+}
 
 // 页面加载完成时触发
 function onLoad() {
-
+    observeElement('#ml-root .ml-list', concatBubble);
 }
 
 
